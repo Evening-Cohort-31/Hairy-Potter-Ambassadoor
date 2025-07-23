d68b2a4ff8833af72f8db233722b77f59598edf0
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-07-23 13:22:35 -0500

    Adding PotteryWheel module and makePottery function

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,11 @@
+const primaryKey = 0;
+
+export const makePottery = (shape, weight, height) => {
+    
+    return {
+        shape: shape,
+        weight: weight,
+        height: height,
+        id: primaryKey += 1
+    }
+}
